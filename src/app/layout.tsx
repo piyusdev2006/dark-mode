e9007e4abaede32032c816c6338881c47e71eb81
piyusdev2006 <piyushsingh955583@gmail.com>
2025-08-26 20:33:50 +0530

    tailwind-css dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased [--pattern-fg: var(--color-neutral-900)]/5 dark:[--pattern-fg:var(--color-neutral-50)]/10`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main className="bg-background text-foreground h-screen">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
