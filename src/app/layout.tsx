344b056a04121f6b5daa47637072a21d1569d00c
piyusdev2006 <piyushsingh955583@gmail.com>
2025-09-04 13:00:17 +0530

    hover focus and other States

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,16 +22,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased [--pattern-fg: var(--color-neutral-900)]/5 dark:[--pattern-fg:var(--color-neutral-50)]/10`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <main className="bg-background text-foreground h-screen">
-            {children}
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+ return (
+   <html lang="en suppressHydrationWarning={true}>">
+     <body
+       className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+       {children}
+     </body>
+   </html>
+ );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import DarkMode from "@/components/darkmode";
-import Image from "next/image";
+import  { Form }  from "@/components/form";
 
 export default function Home() {
-  return <DarkMode/>
+  return (
+    <main className="min-h-screen flex-col max-w-4xl mx-auto flex items-center">
+      <Form />
+    </main>
+  );
 }
-
-
